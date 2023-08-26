d28b89037429c269fce804be2c4340c751a449ea
mayank-thapliyal-1 <111272849+mayank-thapliyal-1@users.noreply.github.com>
2023-08-26 05:46:16 +0530

    second commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,13 +10,12 @@ const Projects=()=>{
 <div id="Projects" className="max-w-[1040] m-auto md:pl-20 p-4 py-16">
     <h1 className="text-4xl font-bold text-center text-[#100b5e]">Projects</h1>
     <p className=" text-center py-8">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dignissimos. Illo quidem numquam modi, itaque asperiores in commodi perspiciatis? Fugiat nesciunt quibusdam possimus. Sunt placeat et, distinctio quo ullam nesciunt.
-    </p>
+    I am a seasoned front-end developer skilled in crafting captivating web experiences. Proficient in HTML, CSS, JavaScript,tailwindcss and ReactJS, I seamlessly blend innovation with functionality.</p>
     <div className="grid sm:grid-cols-2 gap-12">
         <ProjectItem img={QuizzyImg} title='Quizzy' link='https://quizzy-git-main-mayank-thapliyal-1.vercel.app/'/>
-        <ProjectItem img={CodeNebula} title='CodeNebula' link='https://mayank-thapliyal-1.github.io/CodeNebulae/' />
+        <ProjectItem img={CodeNebula} title='CodeNebula' link='https://code-nebulae-git-main-mayank-thapliyal-1.vercel.app/' />
         <ProjectItem img={DrumKitImg} title='DrumKit' link='https://drum-kit-git-main-mayank-thapliyal-1.vercel.app/' />
-        <ProjectItem img={MoveItImg} title='MoveIt' link="https://mayank-thapliyal-1.github.io/Move-IT/"/>
+        <ProjectItem img={MoveItImg} title='MoveIt' link="https://move-it-git-main-mayank-thapliyal-1.vercel.app/"/>
     </div>
  </div>
     );
