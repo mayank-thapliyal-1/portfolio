739a4e09c5b9e7bb18eb226252928628c47d221b
mayank-thapliyal-1 <111272849+mayank-thapliyal-1@users.noreply.github.com>
2023-08-26 23:36:09 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,47 @@
 import React from "react";
 import ProjectItem from "./ProjectItems";
-import QuizzyImg from "../assets/images/Quizzy.png"
-import CodeNebula from "../assets/images/CodeNebula.png"
-import DrumKitImg from "../assets/images/DrumKit.png"
-import MoveItImg from "../assets/images/MoveIt.png"
+import QuizzyImg from "../assets/images/Quizzy.png";
+import CodeNebula from "../assets/images/CodeNebula.png";
+import DrumKitImg from "../assets/images/DrumKit.png";
+import MoveItImg from "../assets/images/Moveit.png";
 
-const Projects=()=>{
-    return(
-<div id="Projects" className=" dark:bg-slate-900 max-w-[1040] m-auto md:pl-20 p-4 py-16">
-    <h1 className="text-4xl font-bold text-center dark:text-slate-200 text-[#100b5e]">Projects</h1>
-    <p className=" dark:text-slate-300 text-center py-8">
-    I am a seasoned front-end developer skilled in crafting captivating web experiences. Proficient in HTML, CSS, JavaScript,tailwindcss and ReactJS, I seamlessly blend innovation with functionality.</p>
-    <div className="grid sm:grid-cols-2 gap-12">
-        <ProjectItem img={QuizzyImg} title='Quizzy' link='https://quizzy-git-main-mayank-thapliyal-1.vercel.app/'/>
-        <ProjectItem img={CodeNebula} title='CodeNebula' link='https://code-nebulae-git-main-mayank-thapliyal-1.vercel.app/' />
-        <ProjectItem img={DrumKitImg} title='DrumKit' link='https://drum-kit-git-main-mayank-thapliyal-1.vercel.app/' />
-        <ProjectItem img={MoveItImg} title='MoveIt' link="https://move-it-git-main-mayank-thapliyal-1.vercel.app/"/>
+const Projects = () => {
+  return (
+    <div
+      id="Projects"
+      className=" dark:bg-slate-900 max-w-[1040] m-auto md:pl-20 p-4 py-16"
+    >
+      <h1 className="text-4xl font-bold text-center dark:text-slate-200 text-[#100b5e]">
+        Projects
+      </h1>
+      <p className=" dark:text-slate-300 text-center py-8">
+        I am a seasoned front-end developer skilled in crafting captivating web
+        experiences. Proficient in HTML, CSS, JavaScript,tailwindcss and
+        ReactJS, I seamlessly blend innovation with functionality.
+      </p>
+      <div className="grid sm:grid-cols-2 gap-12">
+        <ProjectItem
+          img={QuizzyImg}
+          title="Quizzy"
+          link="https://quizzy-git-main-mayank-thapliyal-1.vercel.app/"
+        />
+        <ProjectItem
+          img={CodeNebula}
+          title="CodeNebula"
+          link="https://code-nebulae-git-main-mayank-thapliyal-1.vercel.app/"
+        />
+        <ProjectItem
+          img={DrumKitImg}
+          title="DrumKit"
+          link="https://drum-kit-git-main-mayank-thapliyal-1.vercel.app/"
+        />
+        <ProjectItem
+          img={MoveItImg}
+          title="MoveIt"
+          link="https://move-it-git-main-mayank-thapliyal-1.vercel.app/"
+        />
+      </div>
     </div>
- </div>
-    );
+  );
 };
-export default Projects
+export default Projects;
