9303f27f58c13775125375dbb55aff87e9b79e72
mayank-thapliyal-1 <111272849+mayank-thapliyal-1@users.noreply.github.com>
2023-08-26 23:34:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Sidenav from "./components/Sidenav";
 import Main from "./components/Main";
 import Work from "./components/Work";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Skills from "./components/Skills";
 import DarkModeToggle from "./components/DarkModeToggle";
